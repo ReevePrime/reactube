397085f61c8d092825969291dc38da0860ebaf1c
Samuel Espinho-Alves <samuelespinho@gmail.com>
2021-08-09 12:19:09 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div><SearchBar /></div>
+          );
+    }
+}
+ 
+export default App;
